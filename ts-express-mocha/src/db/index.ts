0cdfccdb6c76801bab1d124128beb7a5cd4aba24
davidivad96 <davidivad96@gmail.com>
2021-12-26 00:58:54 +0100

    Initialize models and create associations

--- ts-express-mocha/src/db/index.ts
+++ ts-express-mocha/src/db/index.ts
@@ -1,8 +1,29 @@
 import { Sequelize } from 'sequelize';
+import UserModelDefiner from './models/user.model';
+import ListModelDefiner from './models/list.model';
+import SongModelDefiner from './models/song.model';
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'sqlite::memory:',
 });
 
-export default sequelize;
+const modelDefiners = [UserModelDefiner, ListModelDefiner, SongModelDefiner];
+
+for (const modelDefiner of modelDefiners) {
+  modelDefiner(sequelize);
+}
+
+(() => {
+  const { User, List, Song } = sequelize.models;
+
+  User.hasMany(List);
+  List.belongsTo(User);
+
+  List.belongsToMany(Song, { through: 'SongList' });
+  Song.belongsToMany(List, { through: 'SongList' });
+})();
+
+const db = { sequelize };
+
+export default db;
