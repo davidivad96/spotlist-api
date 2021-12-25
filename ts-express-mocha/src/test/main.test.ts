6ea466aad74b22050c742c0e0122efddaafcd3b1
davidivad96 <davidivad96@gmail.com>
2021-12-25 15:46:30 +0100

    Install and configure ESLint and Prettier. Format code

--- ts-express-mocha/src/test/main.test.ts
+++ ts-express-mocha/src/test/main.test.ts
@@ -1,7 +1,9 @@
 import { expect } from 'chai';
 
-describe('Main test', () => { // the tests container
-    it('should return 1', () => { // the single test
-        expect(1).to.equal(1);
-    });
+describe('Main test', () => {
+  // the tests container
+  it('should return 1', () => {
+    // the single test
+    expect(1).to.equal(1);
+  });
 });
