04cd0b1e8865b1ea7be138225cc9b8c1ea832bae
davidivad96 <davidivad96@gmail.com>
2021-12-26 00:59:17 +0100

    Connect to database and synchronize before running app

--- ts-express-mocha/src/index.ts
+++ ts-express-mocha/src/index.ts
@@ -1,7 +1,10 @@
 import app from './app';
+import db from './db';
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
 });
