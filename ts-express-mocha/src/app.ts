f8d714642a18c4d9c38ac2932a4d998e6086b75a
davidivad96 <davidivad96@gmail.com>
2021-12-26 23:24:16 +0100

    Add hello world router

--- ts-express-mocha/src/app.ts
+++ ts-express-mocha/src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
+import helloWorldRouter from './routes/hello-world.router';
 import userRouter from './routes/user.router';
 import listRouter from './routes/list.router';
 import songRouter from './routes/song.router';
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(helloWorldRouter);
 app.use(userRouter);
 app.use(listRouter);
 app.use(songRouter);
